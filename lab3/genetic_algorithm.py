793319a4e34c10bb0c36f42776e3ad885dce1fec
Егор Парфинцов <egor.porf1@mail.ru>
2024-05-31 22:30:41 +0300

    lab3: переименовал переменные, сделал population списком

--- lab3/genetic_algorithm.py
+++ lab3/genetic_algorithm.py
@@ -1,67 +1,83 @@
-from random import uniform, random, randint, choices
+from random import uniform, random, randint
+from typing import Callable
+from operator import itemgetter
 
 class GeneticAlgorithm:
-    # Функция приспособленности, количество поколений, вероятность мутации, коэффициент выживаемости, размер популяции.
-    def __init__(self, func, generations=50, min=True, mut_chance=0.8, survive_cof=0.8, pop_number=100):
-        self.func = func
-        self.population = dict()
-        self.mut_chance = mut_chance
-        self.survive_cof = survive_cof
-        self.generations = generations
-        self.pop_number = pop_number
-        self.min_func = min
+    '''Реализация генетического алгоритма Татаряна-Парфинцова'''
+    def __init__(
+            self,
+            fitness          : Callable[[float, float], float], 
+            generations_limit: int   = 50, 
+            is_min           : bool  = True, 
+            p_mutation       : float = 0.8, 
+            p_survive        : float = 0.8, 
+            population_size  : float = 100
+            ):
+        '''
+        fitness          : функция для оптимизации\n
+        generations_limit: кол-во поколений\n
+        is_min           : поиск минимума или нет\n 
+        p_mutation       : вероятность мутации\n
+        p_survive        : коэффициент выживаемости\n 
+        population_size  : размер популяции
+        '''
+        self.fitness = fitness
+        self.population = []
+        self.p_mutation = p_mutation
+        self.p_survive = p_survive
+        self.generations_limit = generations_limit
+        self.population_size = population_size
+        self.is_min = is_min
 
-# Генерирует исходную популяцию особей, каждая из которых представлена двумя генами (x и y)
-# и соответствующим им значением приспособленности.
-    def generate_start_population(self, x, y):
-        for i in range(self.pop_number):
-            po_x = uniform(-x, x)
-            po_y = uniform(-y, y)
-            self.population[i] = [po_x, po_y, self.func(po_x, po_y)]  # Создание начальной популяции
+    def generate_start_population(
+            self, 
+            x_bound: int, 
+            y_bound: int
+            ):
+        '''
+        Генерирует начальную популяцию особей вида [x, y, fitness(x,y)]\n
+        на отрезках [-x_bound, +x_bound] и [-y_bound, +y_bound].
+        '''
+        self.population = [
+            [
+                x:=uniform(-x_bound, x_bound),
+                y:=uniform(-y_bound, y_bound),
+                self.fitness(x, y)
+            ]
+            for key in range(self.population_size)
+        ]
 
-# Возвращает лучших и худших индивидуумов в текущей популяции на основе их показателей физической подготовки.
-    def statistic(self):
-        return [max(self.population.items(), key=lambda item: item[1][2]),
-                min(self.population.items(), key=lambda item: item[1][2])]
+    def best(self):
+        '''
+        Возвращает лучшую особь на основании фитнес-функции
+        '''
+        return min(self.population, key=itemgetter(2)) if self.is_min else max(self.population, key=itemgetter(2))
 
-# Выполняет процесс отбора, который упорядочивает особей в популяции на основе их показателей пригодности
-# и выбирает родителей для следующего поколения.
     def select(self):
-        sorted_pop = dict(
-            sorted(self.population.items(), key=lambda item: item[1][2], reverse=self.min_func))  # Ранжирование
-# Кроссинговер - случайным образом выбираются 2 родителя и создаются 2 ребенка путем обмена их генами.
-        cof = int(self.pop_number * (1 - self.survive_cof))
+        '''
+        Упорядочивает особей в популяции по фитнес-функции\n
+        и выбирает родителей для следующего поколения
+        '''
+        self.population.sort(key=itemgetter(2), reverse=self.is_min)    # ранжирование
 
-        i = 0
-        for pop in sorted_pop.values():
-            parents = choices(list(sorted_pop.values()), k=2)
-            parent1 = parents[0]
-            parent2 = parents[1]
+        # Кроссинговер - случайным образом выбираются 2 родителя и создаются 2 ребенка путем обмена их генами.
+        children_count = int(self.population_size * (1 - self.p_survive))
+        parents = self.population[self.population_size - 2 * children_count:]
+
+        for _, one in enumerate(self.population[:children_count]):
             if random() > 0.5:
-                pop[0] = parent1[0]
-                pop[1] = parent2[1]
-                pop[2] = self.func(pop[0], pop[1])
+                one[0], one[1], one[2] = (x:=parents.pop()[0]), (y:=parents.pop()[1]), self.fitness(x, y)
             else:
-                pop[0] = parent2[0]
-                pop[1] = parent1[1]
-                pop[2] = self.func(pop[0], pop[1])
-            i += 1
-            if i >= cof:
-                break
+                one[1], one[0], one[2] = (y:=parents.pop()[1]), (x:=parents.pop()[0]), self.fitness(x, y)
 
-        self.population = sorted_pop
-
-# Вносятся случайные изменения в гены индивидуумов с определенной вероятностью,
-# что помогает исследовать новые области пространства решений.
     def mutation(self):
-        cof = int(self.pop_number * (1 - self.survive_cof))
-        i = 0
-        for pop in self.population.values():
-            if random() < self.mut_chance:
-                pop[0] += randint(-1, 1)*0.1*pop[0]
-            if random() < self.mut_chance:
-                pop[1] += randint(-1, 1)*0.1*pop[1]
-            pop[2] = self.func(pop[0], pop[1])
-            i += 1
-            if i >= cof:
-                break
+        '''
+        Вносятся случайные изменения в гены с определенной вероятностью,\n
+        что помогает исследовать новые области пространства решений.\n
+        '''
+        for one in self.population:
+            if random() < self.p_mutation:
+                one[0] += randint(-1, 1) * 0.1 * one[0]
+            if random() < self.p_mutation:
+                one[1] += randint(-1, 1) * 0.1 * one[1]
+            one[2] = self.fitness(one[0], one[1])
